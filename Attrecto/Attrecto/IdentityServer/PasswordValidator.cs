f0af4929ef65337007d7d156a9c278ff6d8127ca
Kristóf Trkala <trkala.kristof@bitroll.hu>
2022-07-16 14:13:43 +0200

    added identityserver for authentication

---
+++ Attrecto/Attrecto/IdentityServer/PasswordValidator.cs
@@ -0,0 +1,45 @@
+﻿using Attrecto.Data;
+using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Attrecto.IdentityServer
+{
+    public class PasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly AttrectoTestDbContext dbContext;
+
+        public PasswordValidator(AttrectoTestDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var user = await dbContext.Users.Include(x => x.FkRoleNavigation)
+                .SingleOrDefaultAsync(u => u.Email == context.UserName);
+
+            if (user != null)
+            {
+                if (BCrypt.Net.BCrypt.Verify(context.Password, user.Password))
+                {
+                    context.Result = new GrantValidationResult(
+                        subject: user.IdUser.ToString(),
+                        authenticationMethod: "custom",
+                        claims: new Claim[]
+                        {
+                            new Claim(JwtClaimTypes.Email, user.Email),
+                            new Claim("role", user.FkRoleNavigation.Name)
+                        });
+
+                    return;
+                }
+            }
+
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid username/password");
+            throw new UnauthorizedAccessException("Invalid username/password");
+        }
+    }
+}
--- Attrecto/Attrecto/Program.cs
+++ Attrecto/Attrecto/Program.cs
@@ -1,6 +1,11 @@
 using Attrecto.Data;
+using Attrecto.IdentityServer;
 using Attrecto.Repositories;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +20,46 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AttrectoTestDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
+builder.Services.AddIdentityServer()
+                .AddDeveloperSigningCredential() //This is for dev only scenarios when you don’t have a certificate to use.
+                .AddInMemoryApiResources(IdentityConfig.ApiResources)
+                .AddInMemoryIdentityResources(IdentityConfig.IdentityResources)
+                .AddInMemoryClients(IdentityConfig.Clients)
+                .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+                .AddResourceOwnerValidator<PasswordValidator>();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Audience = "attrecto_api";
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = JwtClaimTypes.Name,
+            ValidateIssuerSigningKey = false,
+            RequireSignedTokens = false,
+            SignatureValidator = delegate (string token, TokenValidationParameters parameters)
+            {
+                var jwt = new JwtSecurityToken(token);
+                return jwt;
+            },
+            ValidateIssuer = false,
+            ValidIssuers = new List<string>() { @"https://localhost:7226" },
+            ValidateAudience = true,
+            ValidAudience = "attrecto_api"
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("DefaultPolicy", options => options.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
+
+    options.DefaultPolicy = options.GetPolicy("DefaultPolicy");
+    options.InvokeHandlersAfterFailure = false;
+});
+
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
@@ -25,7 +70,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseIdentityServer();
+
+app.UseRouting();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
