467353e1fe06a2eccf2371662e54ea2f73ad42b3
Kristóf Trkala <trkala.kristof@bitroll.hu>
2022-07-16 12:27:23 +0200

    base solution with dbcontext

---
+++ Attrecto/Attrecto/Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Attrecto.Data
+{
+    public partial class User
+    {
+        public User()
+        {
+            Todos = new HashSet<Todo>();
+        }
+
+        public int IdUser { get; set; }
+        public int FkRole { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? Name { get; set; }
+
+        public virtual Role FkRoleNavigation { get; set; } = null!;
+        public virtual ICollection<Todo> Todos { get; set; }
+    }
+}
